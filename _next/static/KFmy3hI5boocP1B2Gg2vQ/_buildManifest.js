a02deead0bc4203fc99b9fe1527d2cc68b4fb239
wynekayuliana <wyneka.yuliana@gmail.com>
2023-02-05 18:39:03 +0800

    Updates

---
+++ _next/static/KFmy3hI5boocP1B2Gg2vQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/575-4a868c289924a417.js","static/chunks/pages/index-6858057136b78404.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
